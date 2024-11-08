399a52a26b85b1319713c20d3123ea7a1fbce81c
Enis Hamza <eniss.hamza@gmail.com>
2024-11-09 00:31:54 +0100

    Server.js fix

--- server/server.js
+++ server/server.js
@@ -4,16 +4,17 @@ import { readdirSync } from "fs";
 import mongoose from "mongoose";
 import csrf from "csurf";
 import cookieParser from "cookie-parser";
+import { createServer } from "http";
 
 const morgan = require("morgan");
 require("dotenv").config();
 
 const csrfProtection = csrf({ cookie: true });
 
-//express app
+// Express app
 const app = express();
 
-//databaza
+// Database connection
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -24,23 +25,20 @@ mongoose
   .then(() => console.log("Database Connected Successfully"))
   .catch((err) => console.log("DB Connection Error ->", err));
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 
-//route
+// Routes
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
-//csrf
+// CSRF protection
 app.use(csrfProtection);
-
 app.get("/api/csrf-token", (req, res) => {
   res.json({ csrfToken: req.csrfToken() });
 });
 
-//porti
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// Default handler for serverless deployment
+export default app;
