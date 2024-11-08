5a1f05ebc11a156923ad547d987db30283979faf
Enis Hamza <eniss.hamza@gmail.com>
2024-11-08 22:46:31 +0100

    Initial commit

--- client/server.js
+++ client/server.js
@@ -14,7 +14,7 @@ app
       server.use(
         "/api",
         createProxyMiddleware({
-          target: "http://localhost:8000",
+          target: "https://kursonomia-backend.vercel.app/",
           changeOrigin: true,
         })
       );
@@ -24,7 +24,7 @@ app
     });
     server.listen(3000, (err) => {
       if (err) throw err;
-      console.log("> Ready on http://localhost:8000");
+      console.log("> Ready on https://kursonomia-backend.vercel.app/");
     });
   })
   .catch((err) => {
