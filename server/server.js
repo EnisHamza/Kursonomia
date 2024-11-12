952646b4afbefb230f35a31d71b5c15d9e5d1d9d
Enis Hamza <eniss.hamza@gmail.com>
2024-11-12 17:42:10 +0100

    Try to deploy backend

--- server/server.js
+++ server/server.js
@@ -4,17 +4,16 @@ import { readdirSync } from "fs";
 import mongoose from "mongoose";
 import csrf from "csurf";
 import cookieParser from "cookie-parser";
-import { createServer } from "http";
 
 const morgan = require("morgan");
 require("dotenv").config();
 
 const csrfProtection = csrf({ cookie: true });
 
-// Express app
+//express app
 const app = express();
 
-// Database connection
+//databaza
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -25,20 +24,23 @@ mongoose
   .then(() => console.log("Database Connected Successfully"))
   .catch((err) => console.log("DB Connection Error ->", err));
 
-// Middleware
+//middleware
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 
-// Routes
+//route
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
-// CSRF protection
+//csrf
 app.use(csrfProtection);
+
 app.get("/api/csrf-token", (req, res) => {
   res.json({ csrfToken: req.csrfToken() });
 });
 
-// Default handler for serverless deployment
-export default app;
+//porti
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
